687c2f2fa123878832b655ea8f423c1e1fd6bb5c
brandon-kong <kongbrandon0@gmail.com>
2024-01-07 00:59:59 -0600

    Added login ui, calcuated navbar offset

--- extensions/chrome/src/App.tsx
+++ extensions/chrome/src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
 
-import { Navbar } from '@/components'
+import { Button, H4, Navbar } from '@/components'
 
 function App() {
   return (
     <>
       <Navbar />
+
+      <main className='h-[340px] flex flex-col gap-2 items-center justify-center'>
+        <H4>
+          You are not logged in
+        </H4>
+        <Button>
+        Sign In
+      </Button>
+      </main>  
+      
     </>
   )
 }
