31ffbd1395110c85578105102d79381e651f7b8e
brandon-kong <kongbrandon0@gmail.com>
2024-01-07 18:27:23 -0600

    Added env example

--- extensions/chrome/src/background.ts
+++ extensions/chrome/src/background.ts
@@ -4,6 +4,16 @@ chrome.runtime.onMessage.addListener(async (request) => {
   if (request.action === 'openSignInPage') {
     const url = await getFullUrl();
 
-    chrome.tabs.create({ url: `http://localhost:3000/sign-in?redirect=${url}` });
+    if (!url) return;
+
+    const params = new URLSearchParams();
+    params.set('redirect', url);
+    params.set('extension', 'chrome');
+    params.set('extensionId', chrome.runtime.id);
+
+    params.set('clientId', import.meta.env.VITE_CLIENT_ID);
+    params.set('scope', import.meta.env.VITE_SCOPE);
+    
+    chrome.tabs.create({ url: `${import.meta.env.VITE_HOST_AUTHORIZE_PAGE}?${params.toString()}` });
   }
 });
